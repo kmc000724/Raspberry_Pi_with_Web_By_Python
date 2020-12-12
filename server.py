5b9b7cc172fe01aec2f4c24858e2190877836f7a
kmc970724 <62640358+kmc970724@users.noreply.github.com>
2020-12-12 15:14:42 +0900

    Update server.py

--- server.py
+++ server.py
@@ -122,5 +122,55 @@ def pwm_thread (pin):
         pwm.ChangeDutyCycle (pwm_list[pin][1])
         time.sleep (0.1)
 
+not_available = [1, 2, 4, 6, 9, 14, 17, 20, 25, 27, 28, 30, 34, 39]
+def check_pin_available (pin):
+    for i in not_available:
+        if pin == i:
+            return False
+    return True
+
+@route ("/gpio40")
+def gpio40_index ():
+    for  i in range (1, 41):
+        if (check_pin_available (i) == True):
+            GPIO.setup (i, GPIO.OUT)
+            GPIO.output (i, GPIO.LOW)
+    print ("gpio40_index (\"/\") is invoked ==> ./gpio40x.html will be executed ...")
+    return static_file ("gpio40.html", root = ".")
+
+@route ("/gpio40_output", method = "POST")
+def gpio40_output ():
+    gpio_pin = int (request.forms.get ("pin"))
+    gpio_mode = int (request.forms.get ("mode"))
+
+    GPIO.setup (gpio_pin, GPIO.OUT)
+    GPIO.output (gpio_pin, gpio_mode)
+
+@route ("/gpio40_pwm_ready", method = "POST")
+def gpio40_pwm_ready ():
+    pwm_pin = int (request.forms.get ("pin"))
+
+    if (pwm_list[pwm_pin][0] != None):
+        pwm_list[pwm_pin][4] = False
+
+    pwm_list[pwm_pin][0] = threading.Thread (target = pwm_thread, daemon = True, args = [pwm_pin])
+    pwm_list[pwm_pin][1] = 0
+    pwm_list[pwm_pin][2] = 0
+    pwm_list[pwm_pin][3] = 100
+    pwm_list[pwm_pin][4] = True
+    pwm_list[pwm_pin][0].start ()
+
+@route ("/gpio40_pwm_cdc", method = "POST")
+def gpio40_pwm_cdc ():
+    pwm_pin = int (request.forms.get ("pin"))
+    pwm_value = int (request.forms.get ("pwm"))
+
+    pwm_list[pwm_pin][1] = pwm_value
+
+@route ("/gpio40_pwm_stop", method = "POST")
+def gpio40_pwm_stop ():
+    pwm_pin = int (request.forms.get ("pin"))
+    pwm_list[pwm_pin][4] = False
+
 run (host = "165.229.185.179", port = 80, server = "paste")
 #run (host = "192.168.0.1", port = 80, server = "paste")
